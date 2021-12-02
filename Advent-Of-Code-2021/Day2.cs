2000acd67e2cdbde81b42919903d78014639a513
mitribasseltoubbeh <mitribasseltoubbeh@tamu.edu>
2021-12-02 11:51:46 -0600

    Completed Day 2

---
+++ Advent-Of-Code-2021/Day2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Advent_Of_Code_2021 {
+	class Day2 {
+
+		public static void Part1() {
+
+			long depth = 0, horizontal = 0;
+
+			using (var reader = new StreamReader("Input2.txt")) {
+				while (!reader.EndOfStream) {
+					string[] command = reader.ReadLine().Split(' ');
+					switch (command[0]) {
+						case "up":
+							depth -= Convert.ToInt32(command[1]);
+							break;
+						case "down":
+							depth += Convert.ToInt32(command[1]);
+							break;
+						default:
+							horizontal += Convert.ToInt32(command[1]);
+							break;
+					}
+				}
+			}
+
+			Console.WriteLine(depth * horizontal);
+		}
+
+		public static void Part2() {
+
+			long depth = 0, horizontal = 0, aim = 0;
+
+			using (var reader = new StreamReader("Input2.txt")) {
+				while (!reader.EndOfStream) {
+					string[] command = reader.ReadLine().Split(' ');
+					switch (command[0]) {
+						case "up":
+							aim -= Convert.ToInt32(command[1]);
+							break;
+						case "down":
+							aim += Convert.ToInt32(command[1]);
+							break;
+						default:
+							horizontal += Convert.ToInt32(command[1]);
+							depth += aim * Convert.ToInt32(command[1]);
+							break;
+					}
+				}
+			}
+
+			Console.WriteLine(depth * horizontal);
+		}
+
+	}
+}
--- Advent-Of-Code-2021/Program.cs
+++ Advent-Of-Code-2021/Program.cs
@@ -3,7 +3,7 @@
 namespace Advent_Of_Code_2021 {
 	class Program {
 		static void Main(string[] args) {
-			Day1.Part2();
+			Day2.Part2();
 		}
 	}
 }
