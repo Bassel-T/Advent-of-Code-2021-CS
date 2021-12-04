70a5b7593830701bce65488f29d527f61a21835e
mitribasseltoubbeh <mitribasseltoubbeh@tamu.edu>
2021-12-03 23:53:52 -0600

    Completed Day 4

--- Advent-Of-Code-2021/Program.cs
+++ Advent-Of-Code-2021/Program.cs
@@ -3,7 +3,9 @@
 namespace Advent_Of_Code_2021 {
 	class Program {
 		static void Main(string[] args) {
-			Day3.Part2();
+			Day4.Part1();
+			Console.WriteLine("-----------------");
+			Day4.Part2();
 		}
 	}
 }
